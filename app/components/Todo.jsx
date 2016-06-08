e894df283db1191fc56926287065a949d3b50f72
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-06-08 18:52:03 +0100

    Static to do list

---
+++ app/components/Todo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class Todo extends React.Component {
+  render = () => {
+    let { id, text } = this.props;
+    return (
+      <div>{id}: {text}</div>
+    );
+  }
+}
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import TodoList from 'TodoList';
 
 export default class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Do some more React'
+        },
+        {
+          id: 4,
+          text: 'Wash the dishes'
+        }
+      ]
+    };
+  }
+
   render = () => {
+    let { todos } = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos} />
       </div>
     );
   }

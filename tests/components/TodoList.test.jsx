58da7aeb6603781de3084628d4e0953c79a93c31
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2016-06-08 19:04:39 +0100

    Testing correct number of Todo components are rendered

--- tests/components/TodoList.test.jsx
+++ tests/components/TodoList.test.jsx
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 
-import TodoList from 'Todo';
+import TodoList from 'TodoList';
+import Todo from 'Todo'
 
 describe ('<TodoList />', () => {
   it ('should exist', () => {
     expect(TodoList).toExist();
+  });
+
+  it ('should render one Todo component for each todo item', () => {
+    const TODOS = [
+      {
+        id: 1,
+        text: 'something'
+      },
+      {
+        id: 2,
+        text: 'do something else'
+      }
+    ];
+
+    let todoList = TestUtils.renderIntoDocument(<TodoList todos={TODOS} />);
+    let todosComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+    expect(todosComponents.length).toBe(TODOS.length);
   });
 });
